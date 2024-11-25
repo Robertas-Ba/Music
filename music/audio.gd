e605b5f944c6369df23ba92d5edf057f4f2028ac
C24736925 Robertas Baranauskas <C24736925@myTUDublin.ie>
2024-11-25 14:58:18 +0000

    day1 #1

---
+++ music/audio.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_button_down() -> void:
+	if $AudioStreamPlayer2D.playing:
+		$Button.text = "Play"
+		$AudioStreamPlayer2D.stop()
+	else:
+		$Button.text = "Stop"
+		$AudioStreamPlayer2D.play()
+	pass # Replace with function body.
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	$AudioStreamPlayer2D.pitch_scale = value
+	pass # Replace with function body.
+
+
+
+
+#func _on_h_slider_2_value_changed(value: float) -> void:
+	#var f:AudioEffectPhaser = AudioServer.get_bus_effect()
+	#f.rate_hz = value
+	#pass # Replace with function body.
